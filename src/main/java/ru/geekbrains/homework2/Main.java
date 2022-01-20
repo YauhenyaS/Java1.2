85b2b569db3349bbc9209b6147addc11773b726d
YahenyaS <stasun.evgenya2015@gmail.com>
2022-01-20 20:23:56 +0300

    сс

--- src/main/java/ru/geekbrains/homework2/Main.java
+++ src/main/java/ru/geekbrains/homework2/Main.java
@@ -20,6 +20,9 @@ public class Main {
         int sum=0;
         for(int i=0;i< arr.length;i++){
             for(int j=0; j<arr[i].length;j++) {
+                if(arr.length != 4){
+                    throw new MyArraySizeException();
+                }
                 try {
                     sum += Integer.parseInt(arr[i][j]);
                 } catch(NumberFormatException e) {
